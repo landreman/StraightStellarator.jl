8e52954a2cba4d16391811fc0b27c53c9b3f4a1a
Matt Landreman <mattland@umd.edu>
2024-02-12 10:40:33 -0500

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.48

---
+++ src/StraightStellarator.jl
@@ -0,0 +1,5 @@
+module StraightStellarator
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using StraightStellarator
+using Test
+
+@testset "StraightStellarator.jl" begin
+    # Write your tests here.
+end
